8780f73ca0427c968ab421089230eed3ce863482
Naithar01 <eeung025@gmail.com>
2022-11-23 18:14:40 +0900

    Add sql file, category, post entitys

--- entity/category_entity.go
+++ entity/category_entity.go
@@ -1,12 +1,10 @@
 package entity
 
-import (
-	"time"
-)
+import "time"
 
-type HelloWorld struct {
+type Category struct {
 	Id         int       `json:"id"`
-	World      string    `json:"world"`
+	Title      string    `json:"title"`
 	Created_At time.Time `json:"created_at"`
 	Updated_At time.Time `json:"updated_at"`
 }
---
+++ entity/post_entity.go
@@ -0,0 +1,12 @@
+package entity
+
+import "time"
+
+type Post struct {
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Content     string    `json:"content"`
+	Category_id int       `json:"category_id"` // foreign key (category -> id)
+	Created_At  time.Time `json:"created_at"`
+	Updated_At  time.Time `json:"updated_at"`
+}
